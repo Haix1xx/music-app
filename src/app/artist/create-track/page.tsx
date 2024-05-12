05bfb6f35ea06b2f3ef9c2b39d2d7fac99547b63
Haix1xx <haixinhtuoi123@gmail.com>
2024-05-12 23:32:11 +0700

    update scale

--- src/app/artist/create-track/page.tsx
+++ src/app/artist/create-track/page.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
 import TrackForm from '@/components/Track/TrackForm'
+import { Container } from '@mui/material'
 
 export default function Page() {
-  return <TrackForm />
+  return (
+    <Container sx={{ height: '100%' }}>
+      <TrackForm />
+    </Container>
+  )
 }
